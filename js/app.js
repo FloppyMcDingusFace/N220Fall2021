4bb312450ae1b6284df0a557de47d042c61c2346
FloppyMcDingusFace <chris.rozmin@gmail.com>
2021-09-01 23:11:27 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+function setup () {
+    createCanvas(1000,1000);
+}
+
+function draw() {
+    background(0);
+    circle(500,500,50);
+    circle(750,500,50);
+    circle(250,500,50);
+    fill(255, 255, 255);
+    rect(000,200,1000, 50);
+    rect(000,800,1000, 50);
+    fill(255,255,0);
+    stroke(255,255,255);
+    arc(100, 500, 100, 100, 50, 50);
+    fill(255,255,0)
+}
